e65a692af88bc31fed26d7c7affb45f84103c680
Juan Madurga <jlmadurga@gmail.com>
2016-03-09 16:59:31 +0100

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='microbot',
-    version='0.2.2',
+    version='0.3.0',
     description="""Connect telegram bots to your API""",
     long_description=readme + '\n\n' + history,
     author='Juan Madurga',
