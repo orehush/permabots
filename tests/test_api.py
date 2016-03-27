a5ce32195c17c1fcd50d1d2e3f1bf39f045bc8b4
Juan Madurga <jlmadurga@gmail.com>
2016-03-27 18:53:02 +0200

    API: Handler request url & header parameters. Hook recipients

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,8 @@ from microbot.models import Bot, EnvironmentVar, Handler, Hook, Recipient
 from microbot.test import testcases, factories
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate
-from microbot.views import BotDetail, EnvironmentVarDetail, HandlerDetail, HookDetail
+from microbot.views import BotDetail, EnvironmentVarDetail, HandlerDetail, HookDetail, RecipientDetail,\
+    UrlParameterDetail, HeaderParameterDetail
 from django.conf import settings
 from django.apps import apps
 import json
@@ -299,6 +300,20 @@ class TestHandlerAPI(BaseTestAPI):
             handler_pk = self.handler.pk
         return '%s/bots/%s/handlers/%s/' % (self.api, bot_pk, handler_pk)
     
+    def _handler_url_param_list_url(self, bot_pk=None, handler_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not handler_pk:
+            handler_pk = self.handler.pk
+        return '%s/bots/%s/handlers/%s/urlparams/' % (self.api, bot_pk, handler_pk)    
+    
+    def _handler_header_param_list_url(self, bot_pk=None, handler_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not handler_pk:
+            handler_pk = self.handler.pk
+        return '%s/bots/%s/handlers/%s/headerparams/' % (self.api, bot_pk, handler_pk)            
+    
     def assertHandler(self, pattern, response_text_template, response_keyboard_template, enabled, handler=None):
         if not handler:
             handler = self.handler
@@ -306,6 +321,18 @@ class TestHandlerAPI(BaseTestAPI):
         self.assertEqual(handler.response.text_template, response_text_template)
         self.assertEqual(handler.response.keyboard_template, response_keyboard_template)
         self.assertEqual(handler.enabled, enabled)
+        
+    def assertUrlParam(self, key, value_template, url_param=None):
+        if not url_param:
+            url_param = self.url_param
+        self.assertEqual(url_param.key, key)
+        self.assertEqual(url_param.value_template, value_template)
+
+    def assertHeaderParam(self, key, value_template, header_param=None):
+        if not header_param:
+            header_param = self.header_param
+        self.assertEqual(header_param.key, key)
+        self.assertEqual(header_param.value_template, value_template)
         
     def test_get_handlers_ok(self):
         data = self._test_get_list_ok(self._handler_list_url())
@@ -400,8 +427,193 @@ class TestHandlerAPI(BaseTestAPI):
         
     def test_delete_handler_not_found(self):
         self._test_delete_detail_not_found(self._handler_detail_url(handler_pk=12), HandlerDetail, self.bot.pk, 12)
+
         
+class TestHandlerRequestParamsAPI(BaseTestAPI):
+    
+    def setUp(self):
+        super(TestHandlerRequestParamsAPI, self).setUp()
+        self.handler = factories.HandlerFactory(bot=self.bot)
+        self.url_param = factories.UrlParamFactory(request=self.handler.request)
+        self.header_param = factories.HeaderParamFactory(request=self.handler.request)
+    
+    def _handler_url_param_list_url(self, bot_pk=None, handler_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not handler_pk:
+            handler_pk = self.handler.pk
+        return '%s/bots/%s/handlers/%s/urlparams/' % (self.api, bot_pk, handler_pk)
+    
+    def _handler_url_param_detail_url(self, bot_pk=None, handler_pk=None, url_param_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not handler_pk:
+            handler_pk = self.handler.pk
+        if not url_param_pk:
+            url_param_pk = self.url_param.pk
+        return '%s/bots/%s/handlers/%s/urlparams/%s/' % (self.api, bot_pk, handler_pk, url_param_pk)    
+    
+    def _handler_header_param_list_url(self, bot_pk=None, handler_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not handler_pk:
+            handler_pk = self.handler.pk
+        return '%s/bots/%s/handlers/%s/headerparams/' % (self.api, bot_pk, handler_pk)     
+    
+    def _handler_header_param_detail_url(self, bot_pk=None, handler_pk=None, header_param_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not handler_pk:
+            handler_pk = self.handler.pk
+        if not header_param_pk:
+            header_param_pk = self.header_param.pk
+        return '%s/bots/%s/handlers/%s/headerparams/%s/' % (self.api, bot_pk, handler_pk, header_param_pk)            
         
+    def assertUrlParam(self, key, value_template, url_param=None):
+        if not url_param:
+            url_param = self.url_param
+        self.assertEqual(url_param.key, key)
+        self.assertEqual(url_param.value_template, value_template)
+
+    def assertHeaderParam(self, key, value_template, header_param=None):
+        if not header_param:
+            header_param = self.header_param
+        self.assertEqual(header_param.key, key)
+        self.assertEqual(header_param.value_template, value_template)
+        
+    def test_get_handler_url_params_ok(self):
+        data = self._test_get_list_ok(self._handler_url_param_list_url())
+        self.assertUrlParam(data[0]['key'], data[0]['value_template'])
+        
+    def test_get_handler_url_params_not_auth(self):
+        self._test_get_list_not_auth(self._handler_url_param_list_url())
+        
+    def test_post_handler_url_params_ok(self):
+        data = {'key': self.handler.request.url_parameters.all()[0].key,
+                'value_template': self.handler.request.url_parameters.all()[0].value_template}                         
+        self._test_post_list_ok(self._handler_url_param_list_url(), UrlParam, data)
+        new_url_param = UrlParam.objects.filter(request=self.handler.request)[0]
+        self.assertUrlParam(self.url_param.key, self.url_param.value_template, new_url_param)
+        
+    def test_post_handler_url_params_not_auth(self):
+        data = {'key': self.url_param.key,
+                'value_template': self.url_param.value_template}
+        self._test_post_list_not_auth(self._handler_url_param_list_url(), data)
+            
+    def test_get_handler_url_param_ok(self):
+        data = self._test_get_detail_ok(self._handler_url_param_detail_url())
+        self.assertUrlParam(data['key'], data['value_template'])
+        
+    def test_get_handler_url_param_from_other_bot(self):
+        self._test_get_detail_from_other_bot(self._handler_url_param_detail_url)
+        
+    def test_get_handler_url_param_not_auth(self):
+        self._test_get_detail_not_auth(self._handler_url_param_detail_url())
+        
+    def test_get_handler_url_param_not_found(self):
+        self._test_get_detail_not_found(self._handler_url_param_detail_url(url_param_pk=12))
+        
+    def test_put_handler_url_param_ok(self):
+        data = {'key': self.url_param.key,
+                'value_template': 'new_url_param_value'}
+        self._test_put_detail_ok(self._handler_url_param_detail_url(), data, UrlParameterDetail, self.bot.pk, self.handler.pk, self.url_param.pk)
+        self.assertEqual(UrlParam.objects.get(key=self.url_param.key).value_template, 'new_url_param_value')
+        
+    def test_put_handler_url_param_from_other_bot(self):
+        data = {'key': self.url_param.key,
+                'value_template': 'new_url_param_value'}
+        self._test_put_detail_from_other_bot(self._handler_url_param_detail_url, data, UrlParameterDetail, self.handler.pk, self.url_param.pk)
+        
+    def test_put_handler_url_param_not_auth(self):
+        data = {'key': self.url_param.key,
+                'value_template': 'new_url_param_value'}
+        self._test_put_detail_not_auth(self._handler_url_param_detail_url(), data, UrlParameterDetail, self.bot.pk, self.handler.pk, self.url_param.pk)
+        
+    def test_put_handler_url_param_not_found(self):
+        data = {'key': self.url_param.key,
+                'value_template': 'new_url_param_value'}
+        self._test_put_detail_not_found(self._handler_url_param_detail_url(url_param_pk=12), data, UrlParameterDetail, self.bot.pk, self.handler.pk, 12)
+          
+    def test_delete_handler_url_param_ok(self):
+        self._test_delete_detail_ok(self._handler_url_param_detail_url(), UrlParameterDetail, self.bot.pk, self.handler.pk, self.url_param.pk)
+        self.assertEqual(UrlParam.objects.count(), 0)
+        
+    def test_delete_handler_url_param_from_other_bot(self):
+        self._test_delete_detail_from_other_bot(self._handler_url_param_detail_url, UrlParameterDetail, self.handler.pk, self.url_param.pk)
+        
+    def test_delete_handler_url_param_not_auth(self):
+        self._test_delete_detail_not_auth(self._handler_url_param_detail_url(), UrlParameterDetail, self.bot.pk, self.handler.pk, self.url_param.pk)
+        
+    def test_delete_handler_url_param_not_found(self):
+        self._test_delete_detail_not_found(self._handler_url_param_detail_url(url_param_pk=12), UrlParameterDetail, self.bot.pk, self.handler.pk, 12)
+        
+    def test_get_handler_header_params_ok(self):
+        data = self._test_get_list_ok(self._handler_header_param_list_url())
+        self.assertHeaderParam(data[0]['key'], data[0]['value_template'])
+        
+    def test_get_handler_header_params_not_auth(self):
+        self._test_get_list_not_auth(self._handler_header_param_list_url())
+        
+    def test_post_handler_header_params_ok(self):
+        data = {'key': self.handler.request.header_parameters.all()[0].key,
+                'value_template': self.handler.request.header_parameters.all()[0].value_template}                         
+        self._test_post_list_ok(self._handler_header_param_list_url(), HeaderParam, data)
+        new_header_param = HeaderParam.objects.filter(request=self.handler.request)[0]
+        self.assertHeaderParam(self.header_param.key, self.header_param.value_template, new_header_param)
+        
+    def test_post_handler_header_params_not_auth(self):
+        data = {'key': self.header_param.key,
+                'value_template': self.header_param.value_template}
+        self._test_post_list_not_auth(self._handler_header_param_list_url(), data)
+        
+    def test_get_handler_header_param_ok(self):
+        data = self._test_get_detail_ok(self._handler_header_param_detail_url())
+        self.assertHeaderParam(data['key'], data['value_template'])
+        
+    def test_get_handler_header_param_from_other_bot(self):
+        self._test_get_detail_from_other_bot(self._handler_header_param_detail_url)
+        
+    def test_get_handler_header_param_not_auth(self):
+        self._test_get_detail_not_auth(self._handler_header_param_detail_url())
+        
+    def test_get_handler_header_param_not_found(self):
+        self._test_get_detail_not_found(self._handler_header_param_detail_url(header_param_pk=12))
+        
+    def test_put_handler_header_param_ok(self):
+        data = {'key': self.header_param.key,
+                'value_template': 'new_header_param_value'}
+        self._test_put_detail_ok(self._handler_header_param_detail_url(), data, HeaderParameterDetail, self.bot.pk, self.handler.pk, self.header_param.pk)
+        self.assertEqual(HeaderParam.objects.get(key=self.url_param.key).value_template, 'new_header_param_value')
+        
+    def test_put_handler_header_param_from_other_bot(self):
+        data = {'key': self.header_param.key,
+                'value_template': 'new_header_param_value'}
+        self._test_put_detail_from_other_bot(self._handler_header_param_detail_url, data, HeaderParameterDetail, self.handler.pk, self.header_param.pk)
+        
+    def test_put_handler_header_param_not_auth(self):
+        data = {'key': self.header_param.key,
+                'value_template': 'new_header_param_value'}
+        self._test_put_detail_not_auth(self._handler_header_param_detail_url(), data, HeaderParameterDetail, self.bot.pk, self.handler.pk, self.header_param.pk)
+        
+    def test_put_handler_header_param_not_found(self):
+        data = {'key': self.header_param.key,
+                'value_template': 'new_header_param_value'}
+        self._test_put_detail_not_found(self._handler_header_param_detail_url(header_param_pk=12), data, 
+                                        HeaderParameterDetail, self.bot.pk, self.handler.pk, 12)
+          
+    def test_delete_handler_header_param_ok(self):
+        self._test_delete_detail_ok(self._handler_header_param_detail_url(), HeaderParameterDetail, self.bot.pk, self.handler.pk, self.header_param.pk)
+        self.assertEqual(HeaderParam.objects.count(), 0)
+        
+    def test_delete_handler_header_param_from_other_bot(self):
+        self._test_delete_detail_from_other_bot(self._handler_header_param_detail_url, HeaderParameterDetail, self.handler.pk, self.header_param.pk)
+        
+    def test_delete_handler_header_param_not_auth(self):
+        self._test_delete_detail_not_auth(self._handler_header_param_detail_url(), HeaderParameterDetail, self.bot.pk, self.handler.pk, self.header_param.pk)
+        
+    def test_delete_handler_header_param_not_found(self):
+        self._test_delete_detail_not_found(self._handler_header_param_detail_url(header_param_pk=12), HeaderParameterDetail, self.bot.pk, self.handler.pk, 12)
+    
 class TestHookAPI(BaseTestAPI):
     
     def setUp(self):
@@ -420,7 +632,7 @@ class TestHookAPI(BaseTestAPI):
         if not hook_pk:
             hook_pk = self.hook.pk
         return '%s/bots/%s/hooks/%s/' % (self.api, bot_pk, hook_pk)
-    
+
     def assertHook(self, response_text_template, response_keyboard_template, enabled, recipients, hook=None):
         if not hook:
             hook = self.hook
@@ -512,3 +724,78 @@ class TestHookAPI(BaseTestAPI):
         
     def test_delete_hook_not_found(self):
         self._test_delete_detail_not_found(self._hook_detail_url(hook_pk=12), HookDetail, self.bot.pk, 12)
+
+
+class TestHookRecipientAPI(BaseTestAPI):
+    
+    def setUp(self):
+        super(TestHookRecipientAPI, self).setUp()
+        self.hook = factories.HookFactory(bot=self.bot)
+        self.recipient = factories.RecipientFactory(hook=self.hook)
+        
+    def _hook_recipient_list_url(self, bot_pk=None, hook_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not hook_pk:
+            hook_pk = self.hook.pk
+        return '%s/bots/%s/hooks/%s/recipients/' % (self.api, bot_pk, hook_pk)
+    
+    def _hook_recipient_detail_url(self, bot_pk=None, hook_pk=None, recipient_pk=None):
+        if not bot_pk:
+            bot_pk = self.bot.pk
+        if not hook_pk:
+            hook_pk = self.hook.pk
+        if not recipient_pk:
+            recipient_pk = self.hook.recipients.all()[0].pk
+        return '%s/bots/%s/hooks/%s/recipients/%s/' % (self.api, bot_pk, hook_pk, recipient_pk)
+    
+    def assertRecipient(self, id, recipient=None):
+        if not recipient:
+            recipient = self.hook.recipients.all()[0]
+        self.assertEqual(recipient.id, id)
+
+    def test_get_hook_recipients_ok(self):
+        data = self._test_get_list_ok(self._hook_recipient_list_url())
+        self.assertRecipient(data[0]['id'])
+        
+    def test_get_hook_recipients_not_auth(self):
+        self._test_get_list_not_auth(self._hook_recipient_list_url())
+        
+    def test_post_hook_recipient_ok(self):
+        data = {'id': self.hook.recipients.all()[0].id}                              
+        self._test_post_list_ok(self._hook_recipient_list_url(), Recipient, data)
+        new_recipient = Recipient.objects.filter(hook=self.hook)[0]        
+        self.assertRecipient(self.hook.recipients.all()[0].id, recipient=new_recipient)
+        
+    def test_post_hook_recipient_not_auth(self):
+        data = {'id': self.hook.recipients.all()[0].id}
+        self._test_post_list_not_auth(self._hook_recipient_list_url(), data)
+        
+    def test_get_recipient_ok(self):
+        data = self._test_get_detail_ok(self._hook_recipient_detail_url())
+        self.assertRecipient(data['id'])
+        
+    def test_get_recipient_from_other_bot(self):
+        self._test_get_detail_from_other_bot(self._hook_recipient_detail_url)
+        
+    def test_get_recipient_not_auth(self):
+        self._test_get_detail_not_auth(self._hook_recipient_detail_url())
+        
+    def test_get_recipient_not_found(self):
+        self._test_get_detail_not_found(self._hook_recipient_detail_url(recipient_pk=12))
+          
+    def test_delete_recipient_ok(self):
+        self._test_delete_detail_ok(self._hook_recipient_detail_url(), RecipientDetail, self.bot.pk, self.hook.pk, self.hook.recipients.all()[0].pk)
+        
+    def test_delete_recipient_from_other_bot(self):
+        self._test_delete_detail_from_other_bot(self._hook_recipient_detail_url, RecipientDetail, self.bot.pk, self.hook.pk, self.hook.recipients.all()[0].pk)
+        
+    def test_delete_recipient_not_auth(self):
+        self._test_delete_detail_not_auth(self._hook_recipient_detail_url(), RecipientDetail, self.bot.pk, self.hook.pk, self.hook.recipients.all()[0].pk)
+        
+    def test_delete_recipient_not_found(self):
+        self._test_delete_detail_not_found(self._hook_recipient_detail_url(recipient_pk=12), RecipientDetail, self.bot.pk, self.hook.pk, 12)
+        
+    def test_put_recipient_not_allowed(self):
+        response = self.client.put(self._hook_recipient_detail_url(), HTTP_AUTHORIZATION=self._gen_token(self.bot.owner.auth_token))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
