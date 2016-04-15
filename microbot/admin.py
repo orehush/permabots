42fe70431dbd69f6ebf822cbf0081ade8f431489
Juan Madurga <jlmadurga@gmail.com>
2016-04-15 13:26:19 +0200

    update admin

--- microbot/admin.py
+++ microbot/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from microbot.models import Message, Chat, Update, User, TelegramBot, Handler, EnvironmentVar, Request, Response, Hook, \
-    UrlParam, HeaderParam, TelegramRecipient, State, TelegramChatState
+    UrlParam, HeaderParam, TelegramRecipient, State, TelegramChatState, Bot
 
 admin.site.register(Message)
 admin.site.register(Chat)
 admin.site.register(User)
 admin.site.register(Update)
+admin.site.register(Bot)
 admin.site.register(TelegramBot)
 admin.site.register(Handler)
 admin.site.register(Request)
